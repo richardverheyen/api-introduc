59ae90cb72421ac10a561f5bf1db7d2142a437d0
richardverheyen <richard.verheyen@gmail.com>
2017-05-05 19:23:46 +0100

    RSpec tests passed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,6 @@
 class Profile < ApplicationRecord
+  # model association
+
+  # validations
+  validates_presence_of :user, :lat, :long
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
+# Test suite for the Todo model
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:lat) }
+  it { should validate_presence_of(:long) }
 end
