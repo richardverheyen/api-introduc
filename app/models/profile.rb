5a7fc35e75ddcf616aaeb35868951bae0c067087
richardverheyen <richard.verheyen@gmail.com>
2017-05-24 23:12:52 +0100

    Annotate models before I make structure change

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id         :integer          not null, primary key
+#  user       :string
+#  lat        :float
+#  long       :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Profile < ApplicationRecord
   # model association
 
--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id         :integer          not null, primary key
+#  user       :string
+#  lat        :float
+#  long       :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :profile do
     user { Faker::StarWars.character }
